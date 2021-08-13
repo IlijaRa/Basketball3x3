5c47076ed7cdee4e278cdd10a977cda49eeadb57
IlijaRa <rakocevicilija7@gmail.com>
2021-08-13 16:53:04 +0200

    Created IDataConnections interface, GlobalConfig class and MicrosoftSQLConnector class

---
+++ BasketLibrary/GlobalConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get ; private set; }
+
+        public static void InitializeConnections(bool MicrosoftSQLDB, bool jsonFile)
+        {
+            if (MicrosoftSQLDB)
+            {
+                // TODO: Create the MicrosoftSQL connection
+            }
+
+            if (jsonFile)
+            {
+                // TODO: Create the json file connection
+            }
+        }
+    }
+}
