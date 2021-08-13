02562a43855b9a3eb2073654b9bcac11885a9d4b
IlijaRa <rakocevicilija7@gmail.com>
2021-08-13 23:54:20 +0200

    BasketLibrary project is replaced with the "BasketLibrary" project which runs on .NET Framework 4.7.2 instead of .NET 5.0 Core.

--- Basketball3x3/MainWindow.xaml.cs
+++ Basketball3x3/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BasketLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Basketball3x3
 {
 
@@ -21,6 +23,7 @@ namespace Basketball3x3
         public MainWindow()
         {
             InitializeComponent();
+            GlobalConfig.InitializeConnections(true, true);
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
