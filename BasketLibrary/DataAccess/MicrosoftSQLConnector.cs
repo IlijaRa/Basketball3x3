5f3d178d1893d64dd261a51ffb4fae26cc6f8e04
IlijaRa <rakocevicilija7@gmail.com>
2022-02-26 21:23:51 +0100

    First data insertion in db works (CreatePrize form)

--- BasketLibrary/DataAccess/MicrosoftSQLConnector.cs
+++ BasketLibrary/DataAccess/MicrosoftSQLConnector.cs
@@ -1,6 +1,8 @@
 ﻿using BasketLibrary.Models;
+using Dapper;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +19,21 @@ namespace BasketLibrary.DataAccess
         /// <returns>The prize information, including the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Basketball3x3")))
+            {
+                var p = new DynamicParameters();
+                p.Add("@PlaceNumber", model.PlaceNumber);
+                p.Add("@PlaceName", model.PlaceName);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
+                p.Add("@Id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
 
-            return model;
+                connection.Execute("dbo.spPrize_Insert", p, commandType: CommandType.StoredProcedure);
+
+                model.Id = p.Get<int>("@Id");
+
+                return model;
+            }
         }
     }
 }
